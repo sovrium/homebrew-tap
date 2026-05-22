270feab3846025d28374a0542751585c68cf68c0
Sovrium CI <ci@sovrium.com>
2026-05-22 06:54:57 +0000

    sovrium 0.5.3

--- Formula/sovrium.rb
+++ Formula/sovrium.rb
@@ -7,27 +7,27 @@
 class Sovrium < Formula
   desc "Configuration-driven web application platform"
   homepage "https://sovrium.com"
-  version "0.5.2"
+  version "0.5.3"
   license "BUSL-1.1"
 
   on_macos do
     on_intel do
-      url "https://github.com/sovrium/sovrium/releases/download/v0.5.2/sovrium-0.5.2-darwin-x64.tar.gz"
+      url "https://github.com/sovrium/sovrium/releases/download/v0.5.3/sovrium-0.5.3-darwin-x64.tar.gz"
       sha256 "CHECKSUM_NOT_AVAILABLE"
     end
     on_arm do
-      url "https://github.com/sovrium/sovrium/releases/download/v0.5.2/sovrium-0.5.2-darwin-arm64.tar.gz"
+      url "https://github.com/sovrium/sovrium/releases/download/v0.5.3/sovrium-0.5.3-darwin-arm64.tar.gz"
       sha256 "CHECKSUM_NOT_AVAILABLE"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/sovrium/sovrium/releases/download/v0.5.2/sovrium-0.5.2-linux-x64.tar.gz"
+      url "https://github.com/sovrium/sovrium/releases/download/v0.5.3/sovrium-0.5.3-linux-x64.tar.gz"
       sha256 "CHECKSUM_NOT_AVAILABLE"
     end
     on_arm do
-      url "https://github.com/sovrium/sovrium/releases/download/v0.5.2/sovrium-0.5.2-linux-arm64.tar.gz"
+      url "https://github.com/sovrium/sovrium/releases/download/v0.5.3/sovrium-0.5.3-linux-arm64.tar.gz"
       sha256 "CHECKSUM_NOT_AVAILABLE"
     end
   end
